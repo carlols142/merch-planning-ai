2305e00d88734398f3076befc66b95cc4b29eee4
carlols142 <carlogs0588@gmail.com>
2025-06-02 16:56:43 +1000

    Add files via upload

--- forecast.py
+++ forecast.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from prophet import Prophet
 
-def forecast_demand(df: pd.DataFrame, periods=30):
+def forecast_demand(df: pd.DataFrame, periods=12):
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.groupby(pd.Grouper(key='date', freq='W'))['units_sold'].sum().reset_index()
     df = df.rename(columns={"date": "ds", "units_sold": "y"})
     model = Prophet()
     model.fit(df)
-    future = model.make_future_dataframe(periods=periods)
+    future = model.make_future_dataframe(periods=periods, freq='W')
     forecast = model.predict(future)
-    return forecast[['ds', 'yhat']].tail(periods)
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(periods)
